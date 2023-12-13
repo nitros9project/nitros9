acbbaf555eb2cdd5e0d2088118c1cec89d2efb91
Boisy Pitre <boisy@labmac.local>
2023-12-13 07:21:51 -0600

    Made the copying of Basic09 binaries to F256 and Atari ports much cleaner. Also added the copying of Basic09 source files to disk images for those ports. Added comments to inkey.asm from Robert Doggett's original source Aded miscprog.b09 and mpadd.b09 from https://nitros9.sourceforge.io/wiki/index.php/BASIC09_Programming_Language_Reference_Manual

--- 3rdparty/packages/basic09/inkey.asm
+++ 3rdparty/packages/basic09/inkey.asm
@@ -3,10 +3,18 @@
 *
 * $Id$
 *
+* Called by: RUN INKEY(StrVar)
+*            RUN INKEY(Path, StrVar)
+* Inkey determines if a key has been typed on the given path
+* (Standard Input if not specified), and if so, returns the next
+* character in the String Variable.  If no key has been type, the
+* null string is returned. If a path is specified, it must be
+* either type BYTE or INTEGER.
+*
 * Edt/Rev  YYYY/MM/DD  Modified by
 * Comment
 * ------------------------------------------------------------------
-*          ????/??/??
+*          ????/??/??  Robert Doggett
 * From Tandy OS-9 Level One VR 02.00.00.
 *
 *   1      1998/10/26  Boisy G. Pitre
@@ -14,8 +22,6 @@
 
                     nam       Inkey
                     ttl       Key detect subroutine
-
-* Disassembled 98/09/11 11:55:29 by Disasm v1.6 (C) 1988 by RML
 
                     ifp1
                     use       defsfile
@@ -34,39 +40,47 @@ size                equ       .
 name                fcs       /Inkey/
                     fcb       edition
 
-start               leax      $04,s
-                    ldd       $02,s
-                    cmpd      #$0001
-                    beq       L0033
-                    cmpd      #$0002
-                    bne       L0057
-                    ldd       [<$04,s]
-                    ldx       $06,s
-                    leax      -$01,x
-                    beq       L0031
-                    leax      -$01,x
-                    bne       L0057
-                    tfr       b,a
-L0031               leax      $08,s
-L0033               ldu       $02,x
-                    ldx       ,x
-                    ldb       #$FF
-                    stb       ,x
-                    cmpu      #$0002
-                    bcs       L0043
-                    stb       $01,x
-L0043               ldb       #SS.Ready
-                    os9       I$GetStt
-                    bcs       L0052
-                    ldy       #0001
-                    os9       I$Read
-                    rts
-L0052               cmpb      #E$NotRdy
-                    bne       L0059
-                    rts
-L0057               ldb       #E$ParmEr
-L0059               coma
-                    rts
+                    org       0                   parameters
+Return              rmb       2                   return address of caller
+PCount              rmb       2                   num of parameterss following
+Param1              rmb       2                   1st param address
+Length1             rmb       2                   size
+Param2              rmb       2                   2nd param address
+Length2             rmb       2                   size
+
+start               leax      Param1,s            point to the parameter list
+                    ldd       PCount,s            get the parameter count
+                    cmpd      #1                  just one parameter?
+                    beq       InKey20             ..yes; default path A=0
+                    cmpd      #2                  are there two params?
+                    bne       ParamErr            no, abort
+                    ldd       [Param1,s]          get path number
+                    ldx       Length1,s           and the length
+                    leax      -1,x                byte available?
+                    beq       InKey10             ..yes; (A)=path number
+                    leax      -1,x                integer?
+                    bne       ParamErr            ..no; abort
+                    tfr       b,a                 else transfer B (path) into A
+InKey10             leax      Param2,s            get the address of the second parameter
+InKey20             ldu       2,x                 get the length of string
+                    ldx       ,x                  and the address of string
+                    ldb       #$FF                get initialization value
+                    stb       ,x                  initialize to null string
+                    cmpu      #2                  at least two-byte string?
+                    blo       InKey30             ..no
+                    stb       1,x                 put string terminator
+InKey30             ldb       #SS.Ready           get call code
+                    OS9       I$GetStt            is there any data ready?
+                    bcs       InKey90             ..no; exit
+                    ldy       #1                  read one byte
+                    OS9       I$Read              read it
+                    rts                           return to the caller
+InKey90             cmpb      #E$NotRdy           not ready error?
+                    bne       InKeyErr            error out if not
+                    rts                           else return to caller
+ParamErr            ldb       #E$ParmEr           parameter error
+InKeyErr            coma                          set carry
+                    rts                           return to the caller
 
                     emod
 eom                 equ       *
