e5d64f1f6da98d5825b71c1206ebb40f426f73eb
Boisy Pitre <bpitre@apple.com>
2025-05-05 18:45:55 -0500

    Add Microware comments to rename.asm, reformat to "one space"

--- level1/cmds/rename.asm
+++ level1/cmds/rename.asm
@@ -1,180 +1,183 @@
 ********************************************************************
 * Rename - Rename a file
 *
-* $Id$
-*
 * Edt/Rev  YYYY/MM/DD  Modified by
 * Comment
 * ------------------------------------------------------------------
 *   6      ????/??/??
 * From Tandy OS-9 Level One VR 02.00.00.
+*
+*          2025/05/05  Boisy Pitre
+* Added Microware comments from Soren Roug's Microware source archive.
 
-                    nam       Rename
-                    ttl       Rename a file
+ nam Rename
+ ttl Rename a file
 
-* Disassembled 98/09/11 01:35:46 by Disasm v1.6 (C) 1988 by RML
+ ifp1
+ use defsfile
+ endc
 
-                    ifp1
-                    use       defsfile
-                    endc
+tylg set Prgrm+Objct
+atrv set ReEnt+rev
+rev set $00
+edition set 6
 
-tylg                set       Prgrm+Objct
-atrv                set       ReEnt+rev
-rev                 set       $00
-edition             set       6
+ mod eom,name,tylg,atrv,start,size
 
-                    mod       eom,name,tylg,atrv,start,size
+*
+* Data Storage Definitions
+*
+ org 0
+parmptr rmb 2 directory name ptr
+oldnam rmb 2 old file name ptr
+oldsiz rmb 1 old file name size
+nextparm rmb 2 new file name ptr
+newsiz rmb 2 new file name size
+ rmb 256 stack space
+ rmb 200 room for params
+size equ .
 
-                    org       0
-parmptr             rmb       1
-u0001               rmb       1
-u0002               rmb       2
-u0004               rmb       1
-nextparm            rmb       2
-u0007               rmb       1
-u0008               rmb       24
-pathopts            rmb       26
-u003A               rmb       2
-u003C               rmb       2
-                    rmb       2
-                    rmb       401
-size                equ       .
+name fcs /Rename/
+ fcb edition
 
-name                fcs       /Rename/
-                    fcb       edition
+start cmpd #$0004 at least 3 chars + CR on cmd line?
+ lbcs bpnam branch if less than
+ stx <parmptr save parameter pointer
+* find file
+ lda #WRITE. write mode
+ os9 I$Open open file to rename in write mode
+ bcc RENM10 branch if ok
+ cmpb #E$FNA file not accessible?
+ bne Exit branch if any other error
+ ldx <parmptr else get pointer to file
+ lda #DIR.+WRITE. and try open as directory
+ os9 I$Open try opening again
+ bcs Exit branch if error
+RENM10 stx <nextparm save off updated param pointer
+* get options
+ ldb #SS.Opt get path options
+ leax <PD.OPT,u get direct page option ptr
+ os9 I$GetStt get path options
+ bcs Exit branch if error
+ os9 I$Close close path to file
+ bcs Exit branch if error
+ ldb <PD.DTP get device type
+ cmpb #DT.RBF RBF type device?
+ bne bpnam branch if not
+ bsr PFNS process file names
+ bcs Exit branch if error
+* move data directory to file's directory
+ ldx <oldnam get old name ptr
+ lda #C$CR get carriage return
+ sta -1,x terminate directory name
+ ldx <parmptr get directory name ptr
+ lda #READ.+WRITE. change data directory
+ os9 I$ChgDir change directory
+ bcs Exit branch if errors
+ ldx <nextparm get new name ptr
+ ldb <newsiz+1 get new name size
+ decb get name end offset
+ lda b,x get last name byte
+ ora #$80 set sign
+ sta b,x update byte
+ incb re-adjust name size
+ cmpb <oldsiz names same size?
+ bne RENM20 branch if not
+ leay ,x copy new name ptr
+ ldx <oldnam get old name ptr
+ os9 F$CmpNam compare names
+ bcc RENM30 branch if the same
+RENM20 ldx <nextparm get new name ptr
+ lda #READ.
+ os9 I$Open try to open file
+ bcc bpnam err: name in use
+ cmpb #E$PNNF path name not in use?
+ bne bpnam branch if not
+RENM30 leax <Dot,pcr point to .
+ lda #DIR.!UPDAT. open as directory in update mode
+ os9 I$Open do it!
+ bcs Exit branch if error
+ ldx <PD.DCP get file directory entry ptr
+ ldu <PD.DCP+2
+ os9 I$Seek seek to position
+ bcs Exit branch if error
+ ldx <nextparm get new name ptr
+ ldy <newsiz get new name size
+ os9 I$Write write new name
+ bcs Exit
+ os9 I$Close close directory
+ bcs Exit
+ clrb
+Exit os9 F$Exit
 
-start               cmpd      #$0004              at least 3 chars + CR on cmd line?
-                    lbcs      bpnam               branch if less than
-                    stx       <parmptr            save parameter pointer
-                    lda       #WRITE.             write mode
-                    os9       I$Open              open file to rename in write mode
-                    bcc       L0032               branch if ok
-                    cmpb      #E$FNA              file not accessible?
-                    bne       Exit                branch if any other error
-                    ldx       <parmptr            else get pointer to file
-                    lda       #DIR.+WRITE.        and try open as directory
-                    os9       I$Open              try opening again
-                    bcs       Exit                branch if error
-L0032               stx       <nextparm           save off updated param pointer
-                    ldb       #SS.Opt
-                    leax      <pathopts,u
-                    os9       I$GetStt            get path options
-                    bcs       Exit                branch if error
-                    os9       I$Close             close path to file
-                    bcs       Exit                branch if error
-                    ldb       <pathopts
-                    cmpb      #$01
-                    bne       bpnam
-                    bsr       L00AE
-                    bcs       Exit
-                    ldx       <u0002
-                    lda       #C$CR
-                    sta       -1,x
-                    ldx       <parmptr
-                    lda       #READ.+WRITE.
-                    os9       I$ChgDir
-                    bcs       Exit
-                    ldx       <nextparm
-                    ldb       <u0008
-                    decb
-                    lda       b,x
-                    ora       #$80
-                    sta       b,x
-                    incb
-                    cmpb      <u0004
-                    bne       L0075
-                    leay      ,x
-                    ldx       <u0002
-                    os9       F$CmpNam
-                    bcc       L0082
-L0075               ldx       <nextparm
-                    lda       #READ.
-                    os9       I$Open
-                    bcc       bpnam
-                    cmpb      #E$PNNF
-                    bne       bpnam
-L0082               leax      <Dot,pcr            point to .
-                    lda       #DIR.!UPDAT.        open as directory in update mode
-                    os9       I$Open              do it!
-                    bcs       Exit                branch if error
-                    ldx       <u003A              get file directory entry ptr (PD.DCP)
-                    ldu       <u003C
-                    os9       I$Seek              seek
-                    bcs       Exit                branch if error
-                    ldx       <nextparm           get ptr to name to rename
-                    ldy       <u0007
-                    os9       I$Write
-                    bcs       Exit
-                    os9       I$Close
-                    bcs       Exit
-                    clrb
-Exit                os9       F$Exit
-bpnam               ldb       #E$BPNam
-                    bra       Exit
+bpnam ldb #E$BPNam err: bad path name
+ bra Exit
 
-Dot                 fcc       "."
-                    fcb       C$CR
+Dot fcc "." current directory name
+ fcb C$CR
 
-L00AE               ldx       <parmptr
-                    bsr       L00F6
-                    ldu       <parmptr
-                    lda       ,u
-                    cmpa      #PDELIM
-                    beq       L00C7
-                    lda       ,y
-                    cmpa      #PDELIM
-                    beq       L00C7
-                    leau      <Dot,pcr
-                    stu       <parmptr
-                    bra       L00CD
-L00C7               leax      ,y
-                    bsr       L00F6
-                    bcs       L00F2
-L00CD               stx       <u0002
-                    stb       <u0004
-                    leax      ,y
-                    bsr       L00F6
-                    bcc       L00CD
-                    ldb       <u0004
-                    beq       L00F2
-                    ldx       <nextparm
-                    os9       F$PrsNam
-                    bcs       L00F2
-                    lda       ,y
-                    cmpa      #PDELIM
-                    beq       L00F2
-                    cmpb      #$1E
-                    bcc       L00F2
-                    stx       <nextparm
-                    clra
-                    std       <u0007
-                    rts
-L00F2               comb
-                    ldb       #E$BPNam
-                    rts
-L00F6               os9       F$PrsNam
-                    bcc       L0119
-                    clrb
-                    leau      ,x
-L00FE               lda       ,u+
-                    bpl       L0107
-                    incb
-                    cmpa      #C$PERD!$80
-                    bne       L010C
-L0107               incb
-                    cmpa      #C$PERD
-                    beq       L00FE
-L010C               decb
-                    beq       L0118
-                    leay      -1,u
-                    cmpb      #$03
-                    bcc       L0118
-                    clrb
-                    bra       L0119
-L0118               coma
-L0119               rts
+PFNS ldx <parmptr get directory name ptr
+ bsr RBFPNam parse name
+ ldu <parmptr get directory name ptr
+ lda ,u get first byte
+ cmpa #PDELIM is it pathlist delimiter?
+ beq PFNS10 branch if so
+ lda ,y get next byte
+ cmpa #PDELIM is it pathlist delimiter?
+ beq PFNS10 branch if so
+ leau <Dot,pcr get directory name ptr
+ stu <parmptr s
+ bra PFNS20
+PFNS10 leax ,y move to next name
+ bsr RBFPNam parse name
+ bcs PFNERR branch if error
+PFNS20 stx <oldnam set beginning of last name
+ stb <oldsiz set old name size
+ leax ,y get beginning of next name
+ bsr RBFPNam parse name
+ bcc PFNS20 branch if good name
+ ldb <oldsiz was last name '.' type?
+ beq PFNERR branch if so
+ ldx <nextparm get new name ptr
+ os9 F$PrsNam parse name
+ bcs PFNERR branch if error
+ lda ,y get next byte
+ cmpa #PDELIM is it pathlist delimiter?
+ beq PFNERR branch if so
+ cmpb #30 is name too big?
+ bcc PFNERR branch if so
+ stx <nextparm save new name ptr
+ clra
+ std <newsiz save new name size
+ rts
+ 
+PFNERR comb
+ ldb #E$BPNam err: bad path name
+ rts
+ 
+RBFPNam os9 F$PrsNam is there normal name?
+ bcc RBFPN50 branch if so
+ clrb clear byte count
+ leau ,x copy pathlist ptr
+RBFPN10 lda ,u+ get next character
+ bpl RBFPN20 branch if not last character
+ incb count character
+ cmpa #C$PERD!$80 is it directory name?
+ bne RBFPN30 branch if so
+RBFPN20 incb count character
+ cmpa #C$PERD is it directory name?
+ beq RBFPN10 branch if so
+RBFPN30 decb uncount last character
+ beq RBFPN40 branch if no name
+ leay -1,u get end-of-name ptr
+ cmpb #$03 legal directory name?
+ bcc RBFPN40 branch if not
+ clrb clear carry
+ bra RBFPN50
+RBFPN40 coma set carry
+RBFPN50 rts
 
-                    emod
-eom                 equ       *
-                    end
+ emod
+eom equ *
+ end
 
