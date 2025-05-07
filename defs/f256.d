8cd16b6d6023675073719c8dd61f96d4585e10ae
Roger Taylor <taylor@newfoal.com>
2025-05-06 23:01:51 -0500

    Drop ins

--- defs/f256.d
+++ defs/f256.d
@@ -121,6 +121,16 @@ KrnBlk              SET       $7
                   ENDC
 
 ********************************************************************
+* Custom SetStats for F256
+*
+		    org	      $C0
+SS.FntLoadM	    rmb	      1
+SS.FntLoadF         rmb	      1
+SS.FntChar	    rmb	      1
+SS.SOLIRQ	    rmb	      1
+SS.SOLMUTE	    rmb	      1
+
+********************************************************************
 * System control definitions
 *
 SYS0                equ       $FE00
@@ -268,10 +278,10 @@ KEMP                equ       %00000001
 * 0=System handles x/y  1=harware interprets PS/2 packets
 * Enable is bit 0.  1=show mouse pointer 0 = hide mouse pointer
 MS_MEN		    equ	      $FEA0     mouse mode-enable
-MS_XL		    equ	      $FEA2	mouse x low byte
-MS_XH		    equ	      $FEA3	mouse x high byte	    
-MS_YL		    equ	      $FEA4	mouse y low byte
-MS_YH		    equ	      $FEA5	mouse y high byte
+MS_XH		    equ	      $FEA2	mouse x low byte
+MS_XL		    equ	      $FEA3	mouse x high byte	    
+MS_YH		    equ	      $FEA4	mouse y low byte
+MS_YL		    equ	      $FEA5	mouse y high byte
 MS_PS2B0	    equ	      $FEA6	mouse PS/2 Byte 0
 MS_PS2B1	    equ	      $FEA7	mouse PS/2 Byte 1
 MS_PS2B2	    equ	      $FEA8	mouse PS/2 Byte 2
@@ -488,6 +498,12 @@ TEXT_LUT_FG         equ       $FF00
 TEXT_LUT_BG         equ       $FF40
 
 ********************************************************************
+* F256 font definitions
+FONT_BLK            equ	      $C1
+FONT_0_OFFSET	    equ	      $0000
+FONT_1_OFFSET	    equ	      $0800
+
+********************************************************************
 * F256 SD card interface definitions
 *
 SDC.Base            equ       $FE90
@@ -530,19 +546,19 @@ VKY_TXT_CURSOR_CTRL_REG rmb       1         [0] Enable Text Mode
 VKY_TXT_START_ADD_PTR rmb       1         this is an offset to change the starting address of the text mode Buffer (in X)
 VKY_TXT_CURSOR_CHAR_REG rmb       1
 VKY_TXT_CURSOR_COLR_REG rmb       1
-VKY_TXT_CURSOR_X_REG_L rmb       1
 VKY_TXT_CURSOR_X_REG_H rmb       1
-VKY_TXT_CURSOR_Y_REG_L rmb       1
+VKY_TXT_CURSOR_X_REG_L rmb       1
 VKY_TXT_CURSOR_Y_REG_H rmb       1
+VKY_TXT_CURSOR_Y_REG_L rmb       1
 ; Line interrupt
 VKY_LINE_IRQ_CTRL_REG rmb       1         [0] - enable line 0 - write only
-VKY_LINE_CMP_VALUE_LO rmb       1         write only [7:0]
-VKY_LINE_CMP_VALUE_HI rmb       1         write only [3:0]
+VKY_LINE_CMP_VALUE_HI rmb       1         write only [7:0]
+VKY_LINE_CMP_VALUE_LO rmb       1         write only [3:0]
 
-VKY_PIXEL_X_POS_LO  equ       VKY_LINE_IRQ_CTRL_REG this is where on the video line is the pixel
-VKY_PIXEL_X_POS_HI  equ       VKY_LINE_CMP_VALUE_LO or what pixel is being displayed when the register is read
-VKY_LINE_Y_POS_LO   equ       VKY_LINE_CMP_VALUE_HI this is the line value of the raster
-VKY_LINE_Y_POS_HI   rmb       1
+VKY_PIXEL_X_POS_HI  equ       VKY_LINE_IRQ_CTRL_REG this is where on the video line is the pixel
+VKY_PIXEL_X_POS_LO  equ       VKY_LINE_CMP_VALUE_LO or what pixel is being displayed when the register is read
+VKY_LINE_Y_POS_HI   equ       VKY_LINE_CMP_VALUE_HI this is the line value of the raster
+VKY_LINE_Y_POS_LO   rmb       1
 
 * Text control bit definitions
 Mstr_Ctrl_Text_Mode_En equ       $01       enable the text mode
@@ -576,26 +592,26 @@ TyVKY_BM0_CTRL_REG  equ       $F000
 BM0_Ctrl            equ       $01       enable the BM0
 BM0_LUT0            equ       $02       LUT0
 BM0_LUT1            equ       $04       LUT1
-TyVKY_BM0_START_ADDY_L equ       $F001
+TyVKY_BM0_START_ADDY_H equ       $F001
 TyVKY_BM0_START_ADDY_M equ       $F002
-TyVKY_BM0_START_ADDY_H equ       $F003
+TyVKY_BM0_START_ADDY_L equ       $F003
 ;BM1
 TyVKY_BM1_CTRL_REG  equ       $F008
 BM1_Ctrl            equ       $01       enable the BM0
 BM1_LUT0            equ       $02       LUT0
 BM1_LUT1            equ       $04       LUT1
-TyVKY_BM1_START_ADDY_L equ       $F009
+TyVKY_BM1_START_ADDY_H equ       $F009
 TyVKY_BM1_START_ADDY_M equ       $F00A
-TyVKY_BM1_START_ADDY_H equ       $F00B
+TyVKY_BM1_START_ADDY_L equ       $F00B
 ;BM2
 TyVKY_BM2_CTRL_REG  equ       $F010
 BM2_Ctrl            equ       $01       enable the BM0
 BM2_LUT0            equ       $02       LUT0
 BM2_LUT1            equ       $04       LUT1
 BM2_LUT2            equ       $08       LUT2
-TyVKY_BM2_START_ADDY_L equ       $F011
+TyVKY_BM2_START_ADDY_H equ       $F011
 TyVKY_BM2_START_ADDY_M equ       $F012
-TyVKY_BM2_START_ADDY_H equ       $F013
+TyVKY_BM2_START_ADDY_L equ       $F013
 
 **  THESE ARE DUPLICATES, RECONCILE THIS LATER
 ********************************************************************
@@ -690,19 +706,19 @@ TILE_MAP_ADDY7      equ       $F19C
 
 
 XYMATH_CTRL_REG     equ       $D300     reserved
-XYMATH_ADDY_L       equ       $D301     w
+XYMATH_ADDY_H       equ       $D301     w
 XYMATH_ADDY_M       equ       $D302     w
-XYMATH_ADDY_H       equ       $D303     w
-XYMATH_ADDY_POSX_L  equ       $D304     r/w
-XYMATH_ADDY_POSX_H  equ       $D305     r/w
-XYMATH_ADDY_POSY_L  equ       $D306     r/w
-XYMATH_ADDY_POSY_H  equ       $D307     r/w
-XYMATH_BLOCK_OFF_L  equ       $D308     r only - low block offset
-XYMATH_BLOCK_OFF_H  equ       $D309     r only - hi block offset
+XYMATH_ADDY_L       equ       $D303     w
+XYMATH_ADDY_POSX_H  equ       $D304     r/w
+XYMATH_ADDY_POSX_L  equ       $D305     r/w
+XYMATH_ADDY_POSY_H  equ       $D306     r/w
+XYMATH_ADDY_POSY_L  equ       $D307     r/w
+XYMATH_BLOCK_OFF_H  equ       $D308     r only - low block offset
+XYMATH_BLOCK_OFF_L  equ       $D309     r only - hi block offset
 XYMATH_MMU_BLOCK    equ       $D30A     r only - which mmu block
-XYMATH_ABS_ADDY_L   equ       $D30B     low absolute results
+XYMATH_ABS_ADDY_H   equ       $D30B     low absolute results
 XYMATH_ABS_ADDY_M   equ       $D30C     mid absolute results
-XYMATH_ABS_ADDY_H   equ       $D30D     hi absolute results
+XYMATH_ABS_ADDY_L   equ       $D30D     hi absolute results
 
 ; Sprite block0
 SPRITE_Ctrl_Enable  equ       $01
---
+++ level2/f256/modules/wz.asm
@@ -0,0 +1,66 @@
+********************************************************************
+* WZ - WizFi360 device descriptor
+*
+* $Id$
+*
+* Edt/Rev  YYYY/MM/DD  Modified by
+* Comment
+* ------------------------------------------------------------------
+*   1/1    2025/03/31  Roger Taylor
+
+
+                    nam       WZ
+                    ttl       WizFi360 device descriptor
+
+                    ifp1
+                    use       defsfile
+                    endc
+
+tylg                set       Devic+Objct
+atrv                set       ReEnt+rev
+rev                 set       $00
+
+                    mod       eom,name,tylg,atrv,mgrnam,drvnam
+
+                     fcb       UPDAT.              mode byte
+                    fcb       HW.Page             extended controller address
+                    fdb       $0040               physical controller address
+                    fcb       initsize-*-1        initilization table size
+                    fcb       DT.SCF              device type:0=scf,1=rbf,2=pipe,3=scf
+                    fcb       $00                 case:0=up&lower,1=upper only
+                    fcb       $00                 backspace:0=bsp,1=bsp then sp & bsp
+                    fcb       $00                 delete:0=bsp over line,1=return
+                    fcb       $00                 echo:0=no echo
+                    fcb       $01                 auto line feed:0=off
+                    fcb       $00                 end of line null count
+                    fcb       $01                 pause:0=no end of page pause
+                    fcb       60                  lines per page
+                    fcb       C$BSP               backspace character
+                    fcb       C$DEL               delete line character
+                    fcb       C$CR                end of record character
+                    fcb       C$EOF               end of file character
+                    fcb       C$RPRT              reprint line character
+                    fcb       C$RPET              duplicate last line character
+                    fcb       C$PAUS              pause character
+                    fcb       C$INTR              interrupt character
+                    fcb       C$QUIT              quit character
+                    fcb       C$BSP               backspace echo character
+                    fcb       C$BELL              line overflow character (bell)
+                    fcb       PARNONE             parity
+                    fcb       STOP1+WORD8+B9600   stop bits/word size/baud rate
+                    fdb       name                copy of descriptor name address
+                    fcb       $00		C$XON               acia xon char
+                    fcb       $00		C$XOFF              acia xoff char
+                    fcb       80                  (szx) number of columns for display
+                    fcb       60                  (szy) number of rows for display
+                    fcb       $00                 Extended type
+initsize            equ       *
+
+name                fcs       /wz/
+mgrnam              fcs       /SCF/
+drvnam              fcs       /WizFi/
+
+                    emod
+eom                 equ       *
+                    end
+
