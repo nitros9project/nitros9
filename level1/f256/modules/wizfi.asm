3e7f4a26c1c1ff4a9cde8c1230a2c08f426cd55a
Boisy Gene Pitre <github@pitre.org>
2025-06-13 07:30:30 -0500

    Add code to install ISR

--- level1/f256/modules/wizfi.asm
+++ level1/f256/modules/wizfi.asm
@@ -4,7 +4,19 @@
 
 * Note: we are "stealing" D.SWPage for our driver. This means the WizFi and the
 * SmartWatch functionality are not compatible and should not be in the same bootfile.
-D.WizFi             equ       D.SWPage
+D.WZStatTbl         equ       D.SWPage
+
+* D.WZStatTbl definitions (must be 256 bytes max)
+WZ.StatCnt          equ       4                   there are four channels in the WizFi
+WZ.BufSiz           equ       16                  16 bytes per channel for input buffering
+
+
+* These next value represent offsets into the D.WZStatTbl (256 byte buffer) that the Init routine
+* allocates using F$SRqMem.
+                    org       $00
+WZ.VIRQPkt          rmb       Vi.PkSz             VIRQ variables
+WZ.StatTbl          rmb       WZ.StatCnt*WZ.BufSiz input buffers
+
 
 tylg                set       Drivr+Objct
 atrv                set       ReEnt+rev
@@ -41,8 +53,12 @@ start               lbra      Init              |SCF jump table
 *
 Init
 * Check if we've already allocated memory.
-                    ldx        <D.WizFi
-                    bne        initex
+                    ifgt      Level-1
+                    ldx       <D.WZStatTbl
+                    else
+                    ldx       >D.WZStatTbl
+                    endc
+                    bne       initex
                     
 * Allocate a single 256 byte page of memory
                     ldd       #$0100
@@ -50,8 +66,33 @@ Init
                     os9       F$SRqMem
                     tfr       u,x
                     puls      u
-                    stx       <D.WizFi
+                    ifgt      Level-1
+                    stx       <D.WZStatTbl
+                    else
+                    stx       >D.WZStatTbl
+                    endc
                     
+                    leax      WZ.VIRQPkt,x
+                    pshs      u
+                    tfr       x,u
+                    leax      Vi.Stat,x           ;fake VIRQ status register
+                    lda       #$80                ;VIRQ flag clear, repeated VIRQs
+                    sta       ,x                  ;set it while we're here...
+                    tfr       x,d                 ;copy fake VIRQ status register address
+                    leax      IRQPckt,pcr         ;IRQ polling packet
+                    leay      IRQSvc,pcr          ;IRQ service entry
+                    os9       F$IRQ               ;install
+                    puls      u
+                    bcs       InitEx              ;exit with error
+                    tfr       x,y                 ; move VIRQ software packet to Y
+tryagain
+                    ldx       #$0001              ; code to install new VIRQ
+                    os9       F$VIRQ              ; install
+                    bcc       IRQok               ; no error, continue
+                    cmpb      #E$UnkSvc
+                    bne       InitEx
+IRQok
+
 initex
                     clrb
                     rts
@@ -141,6 +182,15 @@ GetSta              clrb
 SetSta              clrb
                     rts
 
+
+
+* Interrupt Service Routine Section
+IRQPckt             fcb       $00,$01,$0A         ;IRQ packet Flip(1),Mask(1),Priority(1) bytes
+
+* Upon entry, U points to 
+IRQSvc            
+                    rti
+                    
                     emod
 eom                 equ       *
                     end
