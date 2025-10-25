6b850f07a8d9be5fb250a87f5d650cc9c9f188b8
Roger Taylor <taylor@newfoal.com>
2025-10-25 14:03:12 -0500

    Update vtio.asm
    Changes to WM8776 chip Init

--- level1/f256/modules/vtio.asm
+++ level1/f256/modules/vtio.asm
@@ -217,10 +217,11 @@ l@                  cmpa      CODECCtrl,x
 * Initialize the F256 sound hardware.
 InitSound           clr       D.SndPrcID          clear the process ID of the current sound emitter (none)
                     lda       SYS1                get the byte at SYS1
-                    anda      #^SYS_PSG_ST        clear the stereo flag
+*                    anda      #^SYS_PSG_ST clear the stereo flag
+                    ora       #SYS_PSG_ST
                     sta       SYS1                and save it back
 
-InitPSG             pshs      cc                  save the condition code register
+InitPSG             pshs      cc                save the condition code register
                     lda       #$C4                get the sound MMU block
                     orcc      #IntMasks           mask interrupts
                     ldb       MAPSLOT             get the MMU slot we'll map to
@@ -235,26 +236,56 @@ InitPSG             pshs      cc                  save the condition code regist
                     sta       MAPADDR+PSG.Base
                     lda       #%11111111                            set volume of channel to 3
                     sta       MAPADDR+PSG.Base
-                    
+
                     stb       MAPSLOT restore it in the MMU slot
                     puls      cc restore interrupts
 
-InitCODEC           ldx       #CODEC.Base
-                    ldd       #%0000000000011010                    R13 - turn on headphones
-                    bsr       SendToCODEC
-                    ldd       #%0000001100101010                    R21 - enable all analog in
-                    bsr       SendToCODEC
-                    ldd       #%0000000100100011                    R17 - enable all analog in
-                    bsr       SendToCODEC
-                    ldd       #%0000011100101100                    R22 - enable all analog out
-                    bsr       SendToCODEC
-                    ldd       #%0000001000010100                    R10 - DAC interface control
-                    bsr       SendToCODEC
-                    ldd       #%0000001000010110                    R11 - ADC interface control
-                    bsr       SendToCODEC
-                    ldd       #%1101010100011001                    R12 - Master mode control
-                    bsr       SendToCODEC
+* WM8776 CODEC chip registers
+* R00 = [0000000][U][Z][AAAAAAA]             Headphone attenuation: U=Update, Z=Zero Crossing Detection, A=bB 1111001 default for 0dB
+* R10 = [0001010][XXX][DS][0][0][DF]         DS=DAC size 16/20/24/32, DF=DAC Format  Right/Left/I2S/DSP
+* R12 = [0001100][0][0][DAC][0][ADC]         DAC rate  ADC rate, both are custom 101 in original vtio
+* R13 = [0001101][XX][1][XX][H][D][A][C]     Headphones/DAC/ADC/Chip 0=Enabled 1=Muted
+* R22 = [0010110][MUX]                       MUX  Bypass,Aux,DAC (bits 2,1,0)
+* R23 = Write anything to Reset WM8776
 
+InitCODEC
+                    ldx       #CODEC.Base
+                    ldd       #%0010111000000000                    R23 - Reset chip
+
+                    bsr       SendToCODEC
+                    ldd       #%0000000101011000                    R00 - Left Headphone Attenuation Control
+                    bsr       SendToCODEC
+                    ldd       #%0000001101011000                    R01 - Right Headphone Attenuation Control
+
+                    bsr       SendToCODEC
+                    ldd       #%0000011111110000                    R03 - Left DAC Attenuation
+                    bsr       SendToCODEC
+                    ldd       #%0000100111110000                    R04 - Right DAC Attenuation
+
+                    bsr       SendToCODEC
+                    ldd       #%0001010000000010                    R10 - DAC Interface Control 16-bit i2s
+                    bsr       SendToCODEC
+                    ldd       #%0001011000000010                    R11 - ADC Interface Control 
+                    bsr       SendToCODEC
+                    ldd       #%0001100111010101                    R12 - Master Mode Control
+                    bsr       SendToCODEC
+                    ldd       #%0010001100000001                    R17 - ALC Control 2 
+                    bsr       SendToCODEC
+                    ldd       #%0010101011000000                    R21 - ADC Mux Control   Right/Left channels muted
+*                    ldd       #%0010101011011111                    R21 - ADC Mux Control   Right/Left channels muted but AIN#L/AIN#R all enabled
+                    bsr       SendToCODEC
+                    ldd       #%0010110000000001                    R22 - Output Mux MX[2:0] = "001" for DAC-only
+                    bsr       SendToCODEC
+                    bsr       F256Type
+                    cmpa      #$16
+                    beq       CODEC_K2
+CODEC_JR2
+                *     ldd       #%0001101001001010                    R13 - PWR Down Control, Headphones off
+                *     bsr       SendToCODEC
+                *     bra       InitBELL
+CODEC_K2
+                    ldd       #%0001101001000010                    R13 - PWR Down Control, Headphones on
+                    lbsr      SendToCODEC
 InitBELL            leax      Bell,pcr point to the bell emission code
                     stx       >D.Bell   save it in the system global's bell vector
                     rts
