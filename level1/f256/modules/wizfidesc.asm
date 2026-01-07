63036207c2523995e7a540389e6a8c48666cbe9e
Boisy Gene Pitre <github@pitre.org>
2026-01-07 07:43:37 -0600

    Rename WizFi descriptors to wz0-wz3 and utilize wizfidesc.asm Removed z8-z14 descriptors

--- level1/f256/modules/wizfidesc.asm
+++ level1/f256/modules/wizfidesc.asm
@@ -1,40 +1,38 @@
-                    nam       n3
-                    ttl       WizFi360 device descriptor
-
 ********************************************************************
+* wizfidesc - WizFi360 device descriptor
+*
 * Edt/Rev  YYYY/MM/DD  Modified by
 * Comment
 * ------------------------------------------------------------------
-*   1/1    2025/03/31  Roger Taylor
-
 
                     ifp1
                     use       defsfile
                     endc
 
-Base                set       $FF20
-Connection          set       $3
-DeviceMode          set       $08
 tylg                set       Devic+Objct
 atrv                set       ReEnt+rev
-rev                 set       $00
+rev                 set       $07
+
+Base                set       $FF20
+*Connection          set       $0
+*DeviceMode          set       $08
 
                     mod       eom,name,tylg,atrv,mgrnam,drvnam
 
-                    fcb       UPDAT.              mode byte
+                    fcb       UPDAT.       mode byte
                     fcb       HW.Page             extended controller address
                     fdb       Base+Connection+DeviceMode               physical controller address has extra info
                     fcb       initsize-*-1        initilization table size
                     fcb       DT.SCF              device type:0=scf,1=rbf,2=pipe,3=scf
                     fcb       $00                 case:0=up&lower,1=upper only
-                    fcb       $00                 backspace:0=bsp,1=bsp then sp & bsp
+                    fcb       $01                 backspace:0=bsp,1=bsp then sp & bsp
                     fcb       $00                 delete:0=bsp over line,1=return
-                    fcb       $00                 echo:0=no echo
+                    fcb       $01                 echo:0=no echo
                     fcb       $01                 auto line feed:0=off
                     fcb       $00                 end of line null count
-                    fcb       $01                 pause:0=no end of page pause
-                    fcb       60                  lines per page
-                    fcb       C$BSP               backspace character
+                    fcb       $00                 pause:0=no end of page pause
+                    fcb       60                  lines per page (not a safe assumption anymore!)
+                    fcb       C$BSP               backspace character (on most telnet clients)
                     fcb       C$DEL               delete line character
                     fcb       C$CR                end of record character
                     fcb       C$EOF               end of file character
@@ -48,16 +46,22 @@ rev                 set       $00
                     fcb       PARNONE             parity
                     fcb       STOP1+WORD8+1       stop bits/word size/baud rate
                     fdb       name                copy of descriptor name address
-                    fcb       $00		C$XON               acia xon char
-                    fcb       $00		C$XOFF              acia xoff char
+                    fcb       $00                 acia xon char (not used, maybe future assignment?)
+                    fcb       $00                 acia xoff char (not used, maybe future assignment?)
                     fcb       80                  (szx) number of columns for display
                     fcb       60                  (szy) number of rows for display
                     fcb       $00                 Extended type
 initsize            equ       *
 
-name                fcs       /n3/
-mgrnam              fcs       /SCF/
-drvnam              fcs       /WizFi/
+name                equ       *
+ ifeq DeviceMode
+  fcs /wz/
+  else
+                    fcc       /wz/
+                    fcb       Connection+$30+$80
+ endc                    
+mgrnam              fcs       /scf/
+drvnam              fcs       /wizfi/
 
                     emod
 eom                 equ       *
