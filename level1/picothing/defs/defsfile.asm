8562ead5fa266c4f6bbe5af92f9dd6a1e6f68251
Mark Murray <mark@grondar.org>
2026-07-19 10:33:12 +0100

    level1/picothing: add the Pico-Thing Level 1 port
    L1 port sources sharing modules with L2 via the recipes' vpath and
    IFEQ Level-1 conditionals.

---
+++ level1/picothing/defs/defsfile.asm
@@ -0,0 +1,8 @@
+Level               equ       1
+
+                  IFP1
+                    use       /dd/defs/os9.d
+                    use       /dd/defs/rbf.d
+                    use       /dd/defs/scf.d
+                    use       /dd/defs/picothing.d
+                  ENDC
